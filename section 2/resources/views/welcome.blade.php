821c60c01a0b354f42dedda15ce63195e64422f9
dravinpalekar <dravin.palekar@gmail.com>
2023-11-26 16:40:46 +0530

    Done 3 task

---
+++ section 2/resources/views/welcome.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+        
+
+
+        <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+        <!-- Styles -->
+        <link href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.min.css" rel="stylesheet" >
+        
+    </head>
+    <body >
+
+        <div class="table-responsive" >
+            <table id="myTable" class="display">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php 
+                    Use App\Models\products;
+                    $posts = products::all();
+                    // print_r($posts);
+                    foreach ($posts as $value) {
+                    ?>
+                    <tr>
+                        <td><?php echo $value['id'] ?></td>
+                        <td><?php echo $value['name']?></td>
+                        <td><?php echo $value['description'] ?></td>
+                        <td><?php echo $value['price'] ?></td>
+                    </tr>
+                    <?php
+                       # code...
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+        
+        <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js">
+
+            </script>
+            <script >
+                let table = new DataTable('#myTable');
+            </script>
+    </body>
+</html>
